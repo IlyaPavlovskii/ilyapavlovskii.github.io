4d5a08a3d420654214d84913ed9f5e1febf0093f
Ilia Pavlovskii <ilia.pavlovskii@mapbox.com>
2025-05-29 23:37:37 +0200

    Update i18 localization

---
+++ src/i18n/ui.ts
@@ -0,0 +1,23 @@
+export const languages = {
+  en: 'English',
+  ru: 'Русский',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'side_bar.home': 'Home',
+    'side_bar.blog': 'Blog',
+    'side_bar.projects': 'Projects',
+    'side_bar.cv': 'CV',
+    'side_bar.contact': 'Contact',
+  },
+  ru: {
+    'side_bar.home': 'Главная',
+    'side_bar.blog': 'Блог',
+    'side_bar.projects': 'Проекты',
+    'side_bar.cv': 'Резюме',
+    'side_bar.contact': 'Контакт',
+  },
+} as const;
